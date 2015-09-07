96d9d11f6956b7b561dcd9de257d60ca402e9904
Lucas Lima de Araújo <lucaslimapoa2@gmail.com>
2015-09-06 22:44:52 -0300

    Updating Application class to hold information about resources usage, like: cpu, network, ram

--- app/src/main/java/com/tcc/lucas/governorsuggestor/Application.java
+++ app/src/main/java/com/tcc/lucas/governorsuggestor/Application.java
@@ -9,6 +9,12 @@ public class Application {
     private Definitions.ApplicationRank mRank;
     private float mBatteryUsed;
     private float mUsage;
+    private float mRamUsed;
+    private float mCpuUsed;
+    private float mNetworkUsed;
+
+    public Application( ) {
+    }
 
     public Application( String mName, Definitions.ApplicationRank mRank, float mBatteryUsed, float mUsage ){
         this.mName = mName;
@@ -21,31 +27,56 @@ public class Application {
         return mName;
     }
 
-    public void setName(String mName) {
-        this.mName = mName;
-    }
 
     public Definitions.ApplicationRank getRank() {
         return mRank;
-    }
-
-    public void setmRank(Definitions.ApplicationRank mRank) {
-        this.mRank = mRank;
     }
 
     public float getBatteryUsed() {
         return mBatteryUsed;
     }
 
-    public void setBatteryUsed(float mBatteryUsed) {
-        this.mBatteryUsed = mBatteryUsed;
-    }
-
     public float getUsage() {
         return mUsage;
     }
 
+    public float getRamUsed() {
+        return mRamUsed;
+    }
+
+    public float getCpuUsed() {
+        return mCpuUsed;
+    }
+
+    public float getNetworkUsed() {
+        return mNetworkUsed;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
+
+    public void setRank(Definitions.ApplicationRank mRank) {
+        this.mRank = mRank;
+    }
+
+    public void setBatteryUsed(float mBatteryUsed) {
+        this.mBatteryUsed = mBatteryUsed;
+    }
+
     public void setUsage(float mUsage) {
         this.mUsage = mUsage;
+    }
+
+    public void setRamUsed(float mRamUsed) {
+        this.mRamUsed = mRamUsed;
+    }
+
+    public void setCpuUsed(float mCpuUsed) {
+        this.mCpuUsed = mCpuUsed;
+    }
+
+    public void setNetworkUsed(float mNetworkUsed) {
+        this.mNetworkUsed = mNetworkUsed;
     }
 }
